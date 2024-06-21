9d3d34174bf56b4d6e5cc05235dce9f50f114a4f
anta-mbodji <ndeyeantambodji81@gmail.com>
2024-06-21 09:59:22 +0000

    continue

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from Flask import  Flask,redirect,url_for,render_template,request
+from flask import  Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
